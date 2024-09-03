e71e9e01ead2287c5675efa794e174bc42fa66ac
josimarcf2020 <josimarcf2020@gmail.com>
2024-09-03 18:06:29 -0300

    feat/Inclusao de validacao de cadastro

---
+++ CRUDCoreData/CRUDJsonData.swift
@@ -0,0 +1,10 @@
+//
+//  CRUDJsonData.swift
+//  CRUDCoreData
+//
+//  Created by Josimar da Cunha Ferreira on 02/09/24.
+//
+
+import Foundation
+
+
--- CRUDCoreData/ContentView.swift
+++ CRUDCoreData/ContentView.swift
@@ -11,36 +11,53 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(entity: Films.entity(), sortDescriptors: [])
-    
     private var films: FetchedResults<Films>
     @State private var newFilmTitle = ""
+    @State private var selectedFilm: Films?
 
     var body: some View {
-        VStack{
-            TextField("Add new", text: self.$newFilmTitle)
-                .multilineTextAlignment(.center)
-            Button("Save") {
-                self.save()
-                newFilmTitle = ""
-            }
-            List{
-                ForEach(films, id: \.self) { film in
-                    Text("\(film.titulo!)")
+        NavigationView {
+            VStack{
+                TextField("Add new", text: self.$newFilmTitle)
+                    .multilineTextAlignment(.center)
+                Button("Save") {
+                    self.save(film: self.selectedFilm)
+                    newFilmTitle = ""
                 }
-                .onDelete(perform: { indexSet in
-                    for index in indexSet {
-                        self.viewContext.delete(self.films[index])
-                        try? self.viewContext.save()
+                List{
+                    ForEach(films, id: \.self) { 
+                        film in Text(".\(film.titulo!)")
+                            .onTapGesture {
+                                self.newFilmTitle = film.titulo!
+                                self.selectedFilm = film
+                            }
                     }
-                })
+                    .onDelete(perform: { indexSet in
+                        for index in indexSet {
+                            self.viewContext.delete(self.films[index])
+                            try? self.viewContext.save()
+                        }
+                    })
+                }
             }
+            .navigationTitle("Lista de Filmes")
+            .padding()
         }
     }
     
-    func save() {
-        let newFilm = Films(context: self.viewContext)
-        newFilm.titulo = newFilmTitle
-        try? self.viewContext.save()
+    func save(film: Films?) {
+        if self.selectedFilm == nil {
+            if self.newFilmTitle != "" {
+                let newFilm = Films(context: self.viewContext)
+                newFilm.titulo = newFilmTitle
+                try? self.viewContext.save()
+            }
+        } else {
+            film!.titulo = self.newFilmTitle
+            try? viewContext.save()
+            self.newFilmTitle = ""
+            self.selectedFilm = nil
+        };
     }
 
 }
